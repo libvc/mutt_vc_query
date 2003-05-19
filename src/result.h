cc2af16badc13b5d1b92a407cdf4f9a82e7af119
Andrew Hsu <ahsu@users.sf.net>
2003-05-19 08:02:24 +0000

    Doc fixes. Moved the delete function to be public.

--- src/result.h
+++ src/result.h
@@ -1,22 +1,23 @@
 /*
- * mutt_vc_query - vCard query utility for mutt
- * Copyright (C) 2003  Andrew Hsu
+ *  mutt_vc_query - vCard query utility for mutt
+ *  Copyright (C) 2003  Andrew Hsu
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA
  *
- * $Id: result.h,v 1.1 2003/05/13 09:45:15 ahsu Exp $
+ *  $Id: result.h,v 1.2 2003/05/14 03:41:02 ahsu Rel $
  */
 
 #include <stdio.h>
@@ -28,8 +29,8 @@ enum sort_results_by
 
 /*** PROTOTYPES ***/
 query_result *create_query_result ();
+void delete_query_result (query_result * qr);
 void get_results (FILE * fp, const char *query_string, const char *misc_field,
                   int *searched, query_result * results, int *rc);
-void print_results (const query_result * results);
 void sort_results (query_result * results, int n, int sort_by);
-query_result *last_result (query_result * results);
+void print_results (const query_result * results);
